4100ed786ee62f559e9d0c5c75cd55a84683dd0c
wmr1 <wmr@cin.ufpe.br>
2018-01-19 09:01:11 -0300

    Modified main to run LSTM with football game dataset. Added dependencies and files in gitignore.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas
 import math
 import neurallib
+from keras_tqdm import TQDMCallback
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -25,73 +26,80 @@ def create_dataset(dataset, look_back=1):
 	return numpy.array(dataX), numpy.array(dataY)
 
 def main():
-	#load and plot dset
-	headers = ["timestamp", "tagid", "x_pos", "y_pos", "heading", "direction", "energy", "speed", "total_distance"]
-	print("Reading dataset...")
-	dataset = DataSet("./dataset/2013-11-03_tromso_stromsgodset_raw_first.csv", headers, UniformSampling, plot=False)
-	print("Ploting dataset...")
-	print(dataset)
 
 	# dataset = pandas.read_csv('.\dataset\international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
 	# plt.plot(dataset)
 	# plt.show()
 	# dataset = dataset.values
 	# dataset = dataset.astype('float32')
+	#load and plot dset
+	path = "./dataset/2013-11-03_tromso_stromsgodset_raw_first.csv"
+	headers = ["timestamp", "tagid", "x_pos", "y_pos", "heading", "direction", "energy", "speed", "total_distance"]
+	print("Reading dataset...")
+	dataset = pandas.read_csv(path, engine='python', skipfooter=3, names = headers)
+	dataset = dataset[dataset.tagid == 9].filter(items = ['x_pos', 'y_pos'])
 
-	# # normalize the dataset
-	# scaler = MinMaxScaler(feature_range=(0, 1))
-	# dataset = scaler.fit_transform(dataset)
+	#dataset_x = pd.DataFrame(list(map(lambda x,y:[x,y], list(range(dataset.shape[0])), dataset.x_pos)))
+	#dataset_x = dataset_x.astype('float32')
+	dataset_x = list(map(lambda x: [x], dataset.x_pos.values))
 
-	# # split into train and test sets
-	# train_size = int(len(dataset) * 0.67)
-	# test_size = len(dataset) - train_size
-	# train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-	# print(len(train), len(test))
+	#normalize the dataset
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	dataset_x = scaler.fit_transform(dataset_x)
+	# split into train and test sets
+	train_size = int(len(dataset_x) * 0.67)
+	test_size = len(dataset_x) - train_size
+	train, test = dataset_x[0:train_size,:], dataset_x[train_size:len(dataset_x),:]
+	print(len(train), len(test))
 
-	# look_back = 1
-	# trainX, trainY = create_dataset(train, look_back)
-	# testX, testY = create_dataset(test, look_back)
+	look_back = 1	
+	trainX, trainY = create_dataset(train, look_back)
+	testX, testY = create_dataset(test, look_back)
 
-	# # reshape input to be [samples, time steps, features]
-	# trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-	# testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+	print(trainX)
 
-	# # create and fit the LSTM network
-	# model = Sequential()
-	# model.add(LSTM(4, input_shape=(1, look_back)))
-	# model.add(Dense(1))
-	# model.compile(loss='mean_squared_error', optimizer='adam')
-	# model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+	# reshape input to be [samples, time steps, features]
+	trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+	testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 
-	# # make predictions
-	# trainPredict = model.predict(trainX)
-	# testPredict = model.predict(testX)
-	# # invert predictions
-	# trainPredict = scaler.inverse_transform(trainPredict)
-	# trainY = scaler.inverse_transform([trainY])
-	# testPredict = scaler.inverse_transform(testPredict)
-	# testY = scaler.inverse_transform([testY])
-	# # calculate root mean squared error
-	# trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-	# print('Train Score: %.2f RMSE' % (trainScore))
-	# testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-	# print('Test Score: %.2f RMSE' % (testScore))
-	
-	# # shift train predictions for plotting
-	# trainPredictPlot = numpy.empty_like(dataset)
-	# trainPredictPlot[:, :] = numpy.nan
-	# trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
-	# # shift test predictions for plotting
-	# testPredictPlot = numpy.empty_like(dataset)
-	# testPredictPlot[:, :] = numpy.nan
-	# testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
-	# # plot baseline and predictions
-	# plt.plot(scaler.inverse_transform(dataset))
-	# plt.plot(trainPredictPlot)
-	# plt.plot(testPredictPlot)
-	# plt.show()
+	# create and fit the LSTM network
+	model = Sequential()
+	model.add(LSTM(2000, input_shape=(1, look_back)))
+	model.add(Dense(1))
+	model.summary()
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	model.fit(trainX, trainY, epochs=1000, batch_size=1000, verbose=0, callbacks=[TQDMCallback()])
+
+	# make predictions
+	trainPredict = model.predict(trainX)
+	testPredict = model.predict(testX)
+	# invert predictions
+	trainPredict = scaler.inverse_transform(trainPredict)
+	trainY = scaler.inverse_transform([trainY])
+	testPredict = scaler.inverse_transform(testPredict)
+	testY = scaler.inverse_transform([testY])
+	# calculate root mean squared error
+	trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+	print('Train Score: %.2f RMSE' % (trainScore))
+	testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+	print('Test Score: %.2f RMSE' % (testScore))
+
+	# shift train predictions for plotting
+	trainPredictPlot = numpy.empty_like(dataset_x)
+	trainPredictPlot[:, :] = numpy.nan
+	trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+	# shift test predictions for plotting
+	testPredictPlot = numpy.empty_like(dataset_x)
+	testPredictPlot[:, :] = numpy.nan
+	testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset_x)-1, :] = testPredict
+	# plot baseline and predictions
+	plt.plot(scaler.inverse_transform(dataset_x))
+	plt.plot(trainPredictPlot)
+	plt.plot(testPredictPlot)
+	plt.show()
 
 	return
 
 if __name__ == "__main__":
 	main()
+	
